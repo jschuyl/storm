8dc5cd6fd063d926cc1490311ae108f9d9a70241
jschuyl <schuylertjed@gmail.com>
2022-09-12 15:46:03 -0700

    entered a fugue state, but not really the good kind. commented a bunch of code out to help me focus on getting the sidebar to work properly.

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,18 @@ const baseURL = 'https://api.openweathermap.org/data/2.5/onecall';
 const testURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + testLat + '&lon=' + testLon + '&appid=' + APIkey;
 let testLocation = ''
 
+let searchArray = ['phoenix',  'Tucson', 'New York', 'London', 'Tokyo', 'Beijing'];
 
+/*console.log(testURL)
+
+fetch(testURL)
+.then(function (response) {
+    console.log(response.json())
+    if (!response.ok) {
+        alert('Not Working')
+    }
+    console.log(response.status, response.statusText)
+})
 
 function findWeather() {
     var buttonEl = document.getElementById('search-button');
@@ -33,27 +44,42 @@ function findWeather() {
     
 }
 findWeather();
+*/
+function searchHistoryButtonList() {
+    if (searchArray.length > 5) {
+        searchArray.pop()
+    } for (i = 0; i < searchArray.length; i++) {
+        const addBtn = document.querySelector('#sidebar')
+        addBtn.addEventListener('click', addSearchHistoryButton)
+        addBtn.addEventListener('click', buttonLocalStorage) 
+    } 
+    function buttonLocalStorage(event) {
+        const clickedButton = event.target.id;
+        const targetInput = clickedButton.replace('search', 'history');
+        const input = document.getElementById(targetInput);
+        input.value;
+        localStorage.setItem(targetInput, input.value)
+    }
 
-function localStorageList(){
-    searchArray = [];
+    function addSearchHistoryButton() {
+        const newBtn = document.createElement('button');
+        console.log('add')
+        newBtn.innerHTML = searchArray[i];
+        document.body.appendChild(newBtn);
+    }
 }
+searchHistoryButtonList();
+function loadLocalStorage() {
+    for (i = 0; i < timeArray.length; i++) {
 
-
-
+    }
+}
 
 
 
 /*
 console.log(testGeoURL)
 fetch(testGeoURL)
-.then(function (response) {
-    console.log(response.json())
-    if (!response.ok) {
-        alert('Not Working')
-    }
-    console.log(response.status, response.statusText)
-})
-fetch(testURL)
 .then(function (response) {
     console.log(response.json())
     if (!response.ok) {
