12899c8e008412c813b697c58c7f8809b05480c3
jschuyl <schuylertjed@gmail.com>
2022-09-11 20:22:39 -0700

    added a list group to the navbar, I'll get it to update with the search bar later. finally got the api to return a response that wasn't 401 so I started playing around with it to get the search to link in

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,29 @@
 const APIkey = '528234272a9750cafc4559111822d230';
-const baseURL = 'https://api.openweathermap.org/data/2.5/onecall'
+const changeURL = '&exclude=';
+const testLat = '32.21';
+const testLon = '-110.94';
+const baseURL = 'https://api.openweathermap.org/data/2.5/onecall';
+const testURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + testLat + '&lon=' + testLon + '&appid=' + APIkey;
+
+const weatherLocation = document.getElementById('search').value
+const testGeoURL ='http://api.openweathermap.org/geo/1.0/direct?q=' + weatherLocation + '&appid=' + APIkey;
+
+
+
+console.log(testGeoURL)
+fetch(testGeoURL)
+.then(function (response) {
+    console.log(response.json())
+    if (!response.ok) {
+        alert('Not Working')
+    }
+    console.log(response.status, response.statusText)
+})
+fetch(testURL)
+.then(function (response) {
+    console.log(response.json())
+    if (!response.ok) {
+        alert('Not Working')
+    }
+    console.log(response.status, response.statusText)
+})
