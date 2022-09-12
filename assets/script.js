8d5b09f69601ff61dc9450bb8f363d737640de23
jschuyl <schuylertjed@gmail.com>
2022-09-11 21:46:23 -0700

    got input to work with geo api, working on button list next

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,49 @@
-const APIkey = '528234272a9750cafc4559111822d230';
+let APIkey = '528234272a9750cafc4559111822d230';
 const changeURL = '&exclude=';
 const testLat = '32.21';
 const testLon = '-110.94';
 const baseURL = 'https://api.openweathermap.org/data/2.5/onecall';
 const testURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + testLat + '&lon=' + testLon + '&appid=' + APIkey;
-
-const weatherLocation = document.getElementById('search').value
-const testGeoURL ='http://api.openweathermap.org/geo/1.0/direct?q=' + weatherLocation + '&appid=' + APIkey;
+let testLocation = ''
 
 
 
+function findWeather() {
+    var buttonEl = document.getElementById('search-button');
+    buttonEl.addEventListener('click', searchWeather);
+
+    function searchWeather(event) {
+        const clickedButton = event.target.id;
+        const targetInput = clickedButton.replace('-button', '');
+        const locationInput = document.getElementById(targetInput);
+        let testLocation = locationInput.value;
+        console.log(locationInput.value);
+        
+        const geoURL ='http://api.openweathermap.org/geo/1.0/direct?q=' + testLocation + '&appid=' + APIkey;
+
+        fetch(geoURL)
+        .then(function (response) {
+            console.log(response.json())
+            if (!response.ok) {
+                alert('Not Working')
+            }
+            console.log(response.status, response.statusText)
+    })
+    }
+    
+}
+findWeather();
+
+function localStorageList(){
+    searchArray = [];
+}
+
+
+
+
+
+
+/*
 console.log(testGeoURL)
 fetch(testGeoURL)
 .then(function (response) {
@@ -26,4 +60,4 @@ fetch(testURL)
         alert('Not Working')
     }
     console.log(response.status, response.statusText)
-})
+})*/
